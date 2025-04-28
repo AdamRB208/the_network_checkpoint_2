7b7f933c29f71fce241db07d01dbdb5770b4ce44
AdamRB208 <adamrborup@gmail.com>
2025-04-28 12:21:49 -0600

    styled home page and posts card

---
+++ src/components/PostCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { Post } from '@/models/Post.js';
+
+
+defineProps({
+  postProp: { type: Post, required: true }
+})
+
+
+</script>
+
+
+<template>
+  <div class="post-card border border-3 border-networkgrey rounded-4 p-3 mb-4">
+    <img
+      :src="postProp.imgUrl || 'https://media.istockphoto.com/id/2185543591/photo/metallic-broken-chain-symbol-illuminated-by-blue-and-magenta-lights-on-blue-and-magenta.webp?a=1&b=1&s=612x612&w=0&k=20&c=LYp_95t7cM7uAO8jGR6EAS72-rGIz1lZnWq09qV9FbA='"
+      class="w-100 rounded-4 post-img" alt="image for post created by {{ postProp.creator.name }}">
+    <div class="card-body">
+      <hr>
+      <p class="card-text mt-3 d-flex justify-content-center">{{ postProp.body }}</p>
+      <hr>
+      <p class="mb-0 mdi mdi-heart likes-text">{{ postProp.likes.length }}</p>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.post-img {
+  object-fit: cover;
+}
+
+.post-card {
+  background-color: rgba(3, 52, 137, 0.705);
+}
+
+hr {
+  // color: rgb(106, 106, 108), ;
+  border: 2px solid rgb(134, 134, 135);
+  opacity: 100;
+}
+
+.likes-text {
+  color: rgb(96, 96, 97);
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import PostCard from '@/components/PostCard.vue';
 import { postsService } from '@/services/PostsService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
@@ -27,12 +28,21 @@ async function getPosts() {
 
 <template>
   <section class="container">
-    <div class="row">
-      <div v-for="Post in posts" :key="Post.id" class="col-md-10">
-        <div>{{ posts }}</div>
+    <div class="row justify-content-center">
+      <div class="col-12">
+        <h2 class="d-flex justify-content-center m-2">Posts</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div v-for="Post in posts" :key="Post.id" class="col-md-8">
+        <PostCard :postProp="Post" />
       </div>
     </div>
   </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+h2 {
+  color: rgb(134, 134, 135);
+}
+</style>
