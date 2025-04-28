03b031bb63dee52f32326c983477c2d89a571f3e
AdamRB208 <adamrborup@gmail.com>
2025-04-28 10:46:31 -0600

    getting posts

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,23 @@
 <script setup>
+import { postsService } from '@/services/PostsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
 
+onMounted(() => {
+  getPosts()
+})
+
+async function getPosts() {
+  try {
+    await postsService.getPosts()
+  }
+  catch (error) {
+    Pop.error(error, 'could not get posts');
+    logger.log('COULD NOT GET POSTS', error)
+  }
+}
 
 </script>
 
---
+++ src/services/PostsService.js
@@ -0,0 +1,13 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+
+class PostsService {
+  async getPosts() {
+    const response = await api.get('api/posts')
+    logger.log('GOT POSTS', response.data)
+  }
+
+}
+
+export const postsService = new PostsService()
