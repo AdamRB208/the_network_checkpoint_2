c5dfec688b3033548a8759fe6fc60da3d5a7cd87
AdamRB208 <adamrborup@gmail.com>
2025-04-30 12:07:16 -0600

    updated account model / updated layout / styled account page

--- src/models/Account.js
+++ src/models/Account.js
@@ -5,6 +5,10 @@ export class Account {
    * @property {string} email
    * @property {string} name
    * @property {string} picture
+   * @property {string} coverImg
+   * @property {string} bio
+   * @property {string} class
+   * @property {string} graduated
    * 
    * @param {AccountData} data
    */
@@ -13,6 +17,10 @@ export class Account {
     this.email = data.email
     this.name = data.name
     this.picture = data.picture
+    this.coverImg = data.coverImg
+    this.bio = data.bio
+    this.class = data.class
+    this.graduated = data.graduated
     // TODO add additional properties if needed
   }
 }
--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -7,20 +7,48 @@ const account = computed(() => AppState.account)
 </script>
 
 <template>
-  <div class="about text-center">
-    <div v-if="account">
-      <h1>Welcome {{ account.name }}</h1>
-      <img class="rounded" :src="account.picture" alt="" />
-      <p>{{ account.email }}</p>
+  <section class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10 about text-center">
+        <div v-if="account">
+          <div>
+            <h1 class="m-3">Welcome {{ account.name }}</h1>
+            <img :src="account.coverImg" alt="" class="cover-img">
+          </div>
+          <div class="d-flex flex-row align-items-center">
+            <img class="account-img m-5 d-flex justify-content-start" :src="account.picture" alt="">
+            <span class="">
+              <ul>Email: {{ account.email }}</ul>
+              <ul>CodeWorks Class: {{ account.class }}</ul>
+              <ul>Graduated: {{ account.graduated }}</ul>
+              <ul>Links: <i class="mdi mdi-github" type="button"></i><i class="mdi mdi-linkedin" type="button"></i><i
+                  class="mdi mdi-file-account" type="button"></i></ul>
+            </span>
+          </div>
+          <div>
+            <h4>Bio</h4>
+            <p>{{ account.bio }}</p>
+          </div>
+        </div>
+        <div v-else>
+          <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+        </div>
+      </div>
     </div>
-    <div v-else>
-      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
-    </div>
-  </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
-img {
-  max-width: 100px;
+.cover-img {
+  max-width: 100dvh;
+  width: 100%;
+  height: 50dvh;
+  object-fit: cover;
+}
+
+.account-img {
+  height: 8em;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
 }
 </style>
