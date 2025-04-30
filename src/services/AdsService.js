0d35f83382b235d476f21cd2162d67837ef636ce
AdamRB208 <adamrborup@gmail.com>
2025-04-30 10:04:01 -0600

    rendering raw ad pojo

--- src/services/AdsService.js
+++ src/services/AdsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Ad } from "@/models/Ads.js"
+import { AppState } from "@/AppState.js"
 
 class AdsService {
   async getAds() {
     const response = await api.get('api/ads')
     logger.log('Got Ads!', response.data)
+    const ads = response.data.map(pojo => new Ad(pojo))
+    AppState.ad = ads
   }
 }
 
